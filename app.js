695f4f9ebecd703e9f6dc37b37a5b3282ae11ab3
Wesley Whitney <wesleywhitney@gmail.com>
2016-08-09 22:03:34 -0600

    local host hello world

--- app.js
+++ app.js
@@ -56,5 +56,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.get('/', function (req, res) {
+      res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+      console.log('Example app listening on port 3000!');
+});
 
 module.exports = app;
